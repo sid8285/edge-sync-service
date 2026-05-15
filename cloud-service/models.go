f75bce0a76567d322c059adb5458387bfd1e0b6c
Siddhant Srivastava <sid.srivastava@outlook.com>
2026-05-14 23:19:56 -0400

    implementation of cloud-service/storage.go; created functionality for saving transcations and listing them

--- cloud-service/models.go
+++ cloud-service/models.go
@@ -2,9 +2,8 @@ package main
 
 import "time"
 
-
-//Transaction here represents a single retail sale recorded at a store and to the cloud.
-//Both services will use a shape like this so that transactions can travel end to end without needing to be converted.
+// Transaction here represents a single retail sale recorded at a store and to the cloud.
+// Both services will use a shape like this so that transactions can travel end to end without needing to be converted.
 type Transaction struct {
 	ID        string    `json:"id"`
 	StoreID   string    `json:"store_id"`
--- cloud-service/storage.go
+++ cloud-service/storage.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type CloudStore struct {
+	mu           sync.RWMutex
+	transactions map[string]Transaction
+}
+
+func NewCloudStore() *CloudStore {
+	return &CloudStore{
+		transactions: make(map[string]Transaction), //we use make to create new maps bc Go panics at a nil map
+	}
+}
+
+//this function belongs to CloudStore, s is the specific store instance
+func (s *CloudStore) SaveSyncedTransaction(t Transaction) (Transaction, bool, error) {
+	if t.ID == "" {
+		return Transaction{}, false, fmt.Errorf("transaction ID cannot be empty")
+	}
+	s.mu.Lock() //only one writer at a time
+	defer s.mu.Unlock() //unlock regarless of how i return
+
+	existing, alreadyExists := s.transactions[t.ID] //Go's two-value map index. existing maps to whatever was stored under the key, already exists returns a bool - good to know
+	if alreadyExists {
+		return existing, true, nil
+	}
+
+	s.transactions[t.ID] = t
+	return t, false, nil
+}
+
+func (s *CloudStore) ListTransactions() []Transaction {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	transactions := make([]Transaction, 0, len(s.transactions))
+
+	for _, tx := range s.transactions {
+		transactions = append(transactions, tx)
+	}
+
+	return transactions
+}
