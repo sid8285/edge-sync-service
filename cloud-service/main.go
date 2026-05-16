57f3846e81f81cbee1bc7c090e133583a23df8b1
Siddhant Srivastava <sid.srivastava@outlook.com>
2026-05-15 21:35:29 -0400

    wiring up cloud-service in main.go

--- cloud-service/main.go
+++ cloud-service/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
 func main() {
-	// HTTP server will be wired up in Lesson 5.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8081"
+	}
+	addr := ":" + port
+
+	store := NewCloudStore()
+	srv := &server{store: store}
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", srv.handleHealth)
+	mux.HandleFunc("GET /transactions", srv.listTransactions)
+	mux.HandleFunc("POST /transactions/sync", srv.handleSync)
+
+	log.Printf("cloud-service listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
